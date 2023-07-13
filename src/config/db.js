0404595d2e7679278ba4feed652366eb6937f97a
Patrick Paiva <patrickpaiva88@gmail.com>
2023-07-12 21:52:53 -0300

    muda mysql para pool

--- src/config/db.js
+++ src/config/db.js
@@ -10,7 +10,7 @@ module.exports = new Pool({
 
 var mysql = require('mysql');
 
-module.exports = mysql.createConnection({
+module.exports = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
